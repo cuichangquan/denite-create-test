102197fb8d699df53b99949a3c22515ee397ef26
cui_changquan <ccq1170@gmail.com>
2018-07-28 23:42:48 +0900

    save

--- rplugin/python3/denite/source/cui.py
+++ rplugin/python3/denite/source/cui.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # 参考: https://qiita.com/iyuuya/items/8a7e9cc0c9dd6d0e4c32
+# http://koturn.hatenablog.com/category/neovim
 from .base import Base
 
 
@@ -12,7 +13,9 @@ class Source(Base):
         self.kind = 'file'
 
     def on_init(self, context):
+        print('on_init')
 
     def gather_candidates(self, context):
+        return [{'word': "cui_23123123"}, {'word': "chang_666666"}]
 
-    def _convert(self, info):
+    # def _convert(self, info):
