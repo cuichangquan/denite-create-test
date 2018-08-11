3663454d341b9c6de76b9c4f200724cd88eb3794
cui_changquan <ccq1170@gmail.com>
2018-08-12 00:33:14 +0900

    save

--- rplugin/python3/denite/source/cui.py
+++ rplugin/python3/denite/source/cui.py
@@ -11,13 +11,17 @@ import site
 
 logger = logging.getLogger('DeniteSourceCuiLog')
 logger.setLevel(10)
-fh = logging.FileHandler('cui.log')
-logger.addHandler(fh)
 
 # TODO
 # source: log/development.logから以下の情報を取ってくる
 # Log:  GET "/sai/admin" => SessionsController#login_for_org [時間]
 # defualt_action: 「SessionsController#login_for_org」の gfアクション
+
+# このソースをみたほうが早い
+# https://github.com/5t111111/denite-rails/blob/master/rplugin/python3/denite/source/rails.py
+# これを分析してみたほうがいい。
+# https://github.com/Shougo/denite.nvim/blob/master/rplugin/python3/denite/__init__.py
+
 class Source(Base):
 
     def __init__(self, vim):
@@ -30,18 +34,16 @@ class Source(Base):
         context['__cbname'] = cbname
         root_path = util.path2project(self.vim, cbname, context.get('root_markers', ''))
         context['__root_path'] = root_path
-        context['__target_file'] = root_path + 'log/development.log'
+        context['__target_file'] = root_path + '/log/development.log'
+        if 'denite-create-test' in root_path:
+            fh = logging.FileHandler(root_path + '/log/cui.log')
+            logger.addHandler(fh)
+            logger.info(9999)
+
 
     # def on_close(self, context):
-        # TODO
 
-        # このソースをみたほうが早い
-        # https://github.com/5t111111/denite-rails/blob/master/rplugin/python3/denite/source/rails.py
-        # これを分析してみたほうがいい。
-        # https://github.com/Shougo/denite.nvim/blob/master/rplugin/python3/denite/__init__.py
     def gather_candidates(self, context):
-        vim_opened_path = self.vim.command('!git rev-parse --show-toplevel')
-        logger.info(vim_opened_path)
 
         return [
                 {
