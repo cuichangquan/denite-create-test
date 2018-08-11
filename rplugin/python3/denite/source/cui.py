a5fdae481c654f8290e4c0fef8d2497954f15452
cui_changquan <ccq1170@gmail.com>
2018-08-11 22:37:42 +0900

    save

--- rplugin/python3/denite/source/cui.py
+++ rplugin/python3/denite/source/cui.py
@@ -11,8 +11,8 @@ import site
 
 logger = logging.getLogger('DeniteSourceCuiLog')
 logger.setLevel(10)
-# fh = logging.FileHandler('cui.log')
-# logger.addHandler(fh)
+fh = logging.FileHandler('cui.log')
+logger.addHandler(fh)
 
 # TODO
 # source: log/development.logから以下の情報を取ってくる
@@ -28,18 +28,18 @@ class Source(Base):
     def on_init(self, context):
         cbname = self.vim.current.buffer.name
         context['__cbname'] = cbname
-        context['__root_path'] = util.path2project(self.vim, cbname, context.get('root_markers', ''))
-        logger.info(context['__root_path'])
+        root_path = util.path2project(self.vim, cbname, context.get('root_markers', ''))
+        context['__root_path'] = root_path
+        context['__target_file'] = root_path + 'log/development.log'
 
     # def on_close(self, context):
         # TODO
 
-    def gather_candidates(self, context):
         # このソースをみたほうが早い
         # https://github.com/5t111111/denite-rails/blob/master/rplugin/python3/denite/source/rails.py
         # これを分析してみたほうがいい。
         # https://github.com/Shougo/denite.nvim/blob/master/rplugin/python3/denite/__init__.py
-        # git_root = 'git rev-parse --show-toplevel'
+    def gather_candidates(self, context):
         vim_opened_path = self.vim.command('!git rev-parse --show-toplevel')
         logger.info(vim_opened_path)
 
