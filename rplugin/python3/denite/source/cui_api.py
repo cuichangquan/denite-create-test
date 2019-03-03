d888746910f819cd0347dce027b024b732b5cc6a
cui_changquan <ccq1170@gmail.com>
2019-03-03 22:48:13 +0900

    save

--- rplugin/python3/denite/source/cui_api.py
+++ rplugin/python3/denite/source/cui_api.py
@@ -49,7 +49,7 @@ class Source(Base):
         target_lines.reverse()
         return [self._convert(date_time, line) for date_time, line in target_lines]
 
-    # 2019-03-03 11:17:53.541382 I [13042:puma 003] {request_id: d25efb37-436b-4fec-968a-afe1f1f79c9b, user_type: api_call} (29.6ms) Sys::Api::OrderSettlementsController -- Completed #update -- { :controller => "Sys::Api::OrderSettlementsController"・・・ }
+    # 2019-03-03 11:17:53.541382 I [13042:puma 003] {request_id: d25efb37-436b-4fec-968a-afe1f1f79c9b, user_type: api_call} (29.6ms) Sys::Api::OrdersController -- Completed #update -- { :controller => "Sys::Api::OrdersController"・・・ }
     def _find_lines(self, lines):
         target_lines = []
         for line in lines:
@@ -79,25 +79,3 @@ class Source(Base):
         conroller_name = inflection.underscore(conroller_name).replace('::', '/')
         return self.root_path + '/app/controllers/' + conroller_name + '.rb'
 
-
-# https://www.jdoodle.com/python3-programming-online
-# import re
-# request_path_pattern = re.compile(",\s:path\s=>\s\"(.*)\",")
-# request_completed_pattern = re.compile("request_id: [a-z0-9-]{36}.*ms\)\s(.*Controller)\s--\sCompleted\s#(.*)\s--\s(.*)")
-# line = '2019-03-03 11:17:53.541382 I [13042:puma 003] {request_id: d25efb37-436b-4fec-968a-afe1f1f79c9b, user_type: api_call} (29.6ms) Sys::Api::OrderSettlementsController -- Completed #update -- { :controller => "Sys::Api::OrderSettlementsController", :action => "update", :params => { "settlement" => { "settle_no" => "5555", "expiration_date" => "2019-08-06", "payment_url" => "http://localhost" }, "order_id" => "3", "order_settlement" => { "settlement" => { "settle_no" => "5555", "expiration_date" => "2019-08-06", "payment_url" => "http://localhost" } } }, :format => "*/*", :method => "PATCH", :path => "/sys/orders/3/settlement", :status => 200, :view_runtime => 1.71, :db_runtime => 16.05, :status_message => "OK" }'
-# result = request_completed_pattern.search(line)
-# print(result[0])
-# print(result[1])
-# print(result[2])
-# print(result[3])
-# request_path=request_path_pattern.search(line)
-# print(request_path[0])
-# print(request_path[1])
-
-# 結果
-# request_id: d25efb37-436b-4fec-968a-afe1f1f79c9b, user_type: api_call} (29.6ms) Sys::Api::OrderSettlementsController -- Completed #update -- { :controller => "Sys::Api::OrderSettlementsController", :action => "update", :params => { "settlement" => { "settle_no" => "5555", "expiration_date" => "2019-08-06", "payment_url" => "http://localhost" }, "order_id" => "3", "order_settlement" => { "settlement" => { "settle_no" => "5555", "expiration_date" => "2019-08-06", "payment_url" => "http://localhost" } } }, :format => "*/*", :method => "PATCH", :path => "/sys/orders/3/settlement", :status => 200, :view_runtime => 1.71, :db_runtime => 16.05, :status_message => "OK" }
-# Sys::Api::OrderSettlementsController
-# update
-# { :controller => "Sys::Api::OrderSettlementsController", :action => "update", :params => { "settlement" => { "settle_no" => "5555", "expiration_date" => "2019-08-06", "payment_url" => "http://localhost" }, "order_id" => "3", "order_settlement" => { "settlement" => { "settle_no" => "5555", "expiration_date" => "2019-08-06", "payment_url" => "http://localhost" } } }, :format => "*/*", :method => "PATCH", :path => "/sys/orders/3/settlement", :status => 200, :view_runtime => 1.71, :db_runtime => 16.05, :status_message => "OK" }
-# , :path => "/sys/orders/3/settlement",
-# /sys/orders/3/settlement
